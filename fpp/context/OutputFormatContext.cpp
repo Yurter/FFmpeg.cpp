761ab780a19543eda583b03a840b31ab43162ca7
Yurter <sterben_idd@mail.ru>
2020-03-20 10:18:08 +0300

    [Temp] Crutch for video/audio rtp stream

--- fpp/context/OutputFormatContext.cpp
+++ fpp/context/OutputFormatContext.cpp
@@ -144,6 +144,9 @@ namespace fpp {
         }
         output_params->completeFrom(input_params);
         const auto created_stream { createStream(output_params) };
+        if (output_params->isAudio()) { // TODO rtp cructh 19.03
+            created_stream->raw()->index = 1;
+        }
         if (full_stream_copy) {
             if (const auto ret {
                 ::avcodec_parameters_copy(
