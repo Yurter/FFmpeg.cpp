c38789d884f0d0a340635849ebca5be24be8dfc5
Yurter <sterben_idd@mail.ru>
2020-03-23 11:47:28 +0300

    [Refactor] main

--- main.cpp
+++ main.cpp
@@ -12,98 +12,121 @@ extern "C" {
     #include <libavdevice/avdevice.h>
 }
 
-/*
+/* USB video
 video=HP Wide Vision FHD Camera
+video=HP Wide Vision HD
 video=Webcam C170
 video=USB2.0 PC CAMERA
-rtsp://admin:admin@192.168.10.189:554/ch01.264
-rtsp://admin:Admin2019@192.168.10.12:554
-rtsp://admin:admin@192.168.10.3:554 (1080p)
 */
 
-void simpleCopyFile() {
+/* USB audio
+audio=Набор микрофонов (Realtek High Definition Audio)
+*/
+
+/* RTSP
+rtsp://admin:admin@192.168.10.189:554/ch01.264
+rtsp://admin:Admin2019@192.168.10.12:554
+rtsp://admin:admin@192.168.10.3:554 (1080p + audio)
+*/
+
+void transmuxing_file() {
 
     /* create source */
-    fpp::InputFormatContext camera { "rtsp://admin:admin@192.168.10.3:554" };
+    fpp::InputFormatContext source {
+        "rtsp://admin:admin@192.168.10.3:554"
+    };
 
     /* open source */
-    camera.open();
+    source.open();
 
     /* create sink */
-    fpp::OutputFormatContext youtube { "rtsp_copy.flv" };
+    fpp::OutputFormatContext sink {
+        "copy.flv"
+    };
 
     /* copy source's streams to sink */
-    for (const auto& input_stream : camera.streams()) {
-        youtube.copyStream(input_stream);
+    for (const auto& input_stream : source.streams()) {
+        sink.copyStream(input_stream);
     }
 
     /* open sink */
-    youtube.open();
+    sink.open();
 
-    /* set timeout */
-    camera.stream(fpp::MediaType::Video)->setEndTimePoint(1 * 60 * 1000); // 1 min
+    /* set timeout (because of endless rtsp stream) */
+    const auto one_minute { 1 * 60 * 1000 };
+    source.stream(fpp::MediaType::Video)->setEndTimePoint(one_minute);
 
-    fpp::Packet input_packet { fpp::MediaType::Unknown };
+    fpp::Packet input_packet {
+        fpp::MediaType::Unknown
+    };
     const auto read_packet {
-        [&input_packet,&camera]() {
-            input_packet = camera.read();
+        [&input_packet,&source]() {
+            input_packet = source.read();
             return !input_packet.isEOF();
         }
     };
 
     /* read and write packets */
     while (read_packet()) {
-        youtube.write(input_packet);
+        sink.write(input_packet);
     }
+
+    /* explicitly close contexts */
+    source.close();
+    sink.close();
 
 }
 
-void startYoutubeStream() {
-#define FPP_DEBUG
+void youtube_stream() {
 
     /* create source */
-    fpp::InputFormatContext camera { "rtsp://admin:admin@192.168.10.3:554" };
+    fpp::InputFormatContext source {
+        "rtsp://admin:admin@192.168.10.3:554"
+    };
 
     /* open source */
-    camera.open();
+    source.open();
 
     /* create sink */
-#ifndef FPP_DEBUG
-    const std::string stream_key { "apaj-8d8z-0ksj-6qxe" };
+    const std::string stream_key {
+        "aaaa-bbbb-cccc-dddd"
+    };
     fpp::OutputFormatContext youtube {
         "rtmp://a.rtmp.youtube.com/live2/"
         + stream_key
     };
-#endif
-#ifdef FPP_DEBUG
-    fpp::OutputFormatContext youtube { "youtube.flv" };
-#endif
 
     /* copy source's streams to sink */
-    for (const auto& input_stream : camera.streams()) {
+    for (const auto& input_stream : source.streams()) {
         if (input_stream->isVideo()) {
-            youtube.copyStream(
-                input_stream, fpp::utils::make_youtube_video_params()
+            youtube.copyStream( /* with predefined params */
+                input_stream
+                , fpp::utils::make_youtube_video_params()
             );
         }
-        if (input_stream->isAudio()) {
-            youtube.copyStream(
-                input_stream, fpp::utils::make_youtube_audio_params()
+        else if (input_stream->isAudio()) {
+            youtube.copyStream( /* with predefined params */
+                input_stream
+                , fpp::utils::make_youtube_audio_params()
             );
         }
     }
 
     /* create decoders */
-    fpp::DecoderContext video_decoder { camera.stream(fpp::MediaType::Video) };
-    fpp::DecoderContext audio_decoder { camera.stream(fpp::MediaType::Audio) };
+    fpp::DecoderContext video_decoder {
+        source.stream(fpp::MediaType::Video)
+    };
+    fpp::DecoderContext audio_decoder {
+        source.stream(fpp::MediaType::Audio)
+    };
 
     /* create encoder's options */
     fpp::Options video_options {
-        { "threads",        "1"     }
-        , { "thread_type",  "slice" }
-        , { "preset",       "fast"  }
-        , { "crf",          "30"    }
-        , { "profile",      "main"  }
+        { "threads",        "1"          }
+        , { "thread_type",  "slice"       }
+        , { "preset",       "ultrafast"   }
+        , { "crf",          "30"          } // 0-51
+        , { "profile",      "main"        }
         , { "tune",         "zerolatency" }
     };
 
@@ -112,36 +135,35 @@ void startYoutubeStream() {
     };
 
     /* create encoders */
-    fpp::EncoderContext video_encoder { youtube.stream(fpp::MediaType::Video), video_options };
-    fpp::EncoderContext audio_encoder { youtube.stream(fpp::MediaType::Audio), audio_options };
+    fpp::EncoderContext video_encoder {
+        youtube.stream(fpp::MediaType::Video), video_options
+    };
+    fpp::EncoderContext audio_encoder {
+        youtube.stream(fpp::MediaType::Audio), audio_options
+    };
 
     /* create resampler */
-    fpp::ResampleContext resample {
-        { camera.stream(fpp::MediaType::Audio)->params
-        , youtube.stream(fpp::MediaType::Audio)->params }
-    };
+    fpp::ResampleContext resample {{
+        source.stream(fpp::MediaType::Audio)->params
+        , youtube.stream(fpp::MediaType::Audio)->params
+    }};
 
     /* open sink */
     youtube.open();
 
-    const auto transcode_video {
-        fpp::utils::transcoding_required(
-            { camera.stream(fpp::MediaType::Video)->params
-            , youtube.stream(fpp::MediaType::Video)->params })
+    const auto video_transcoding_required {
+        fpp::utils::transcoding_required({
+            source.stream(fpp::MediaType::Video)->params
+            , youtube.stream(fpp::MediaType::Video)->params
+        })
     };
 
-    /* set timeout */
-#ifndef FPP_DEBUG
-    camera.stream(fpp::MediaType::Video)->setEndTimePoint(10 * 60 * 1000); // 10 min
-#endif
-#ifdef FPP_DEBUG
-    camera.stream(fpp::MediaType::Video)->setEndTimePoint(20 * 1000); // 20 sec
-#endif
-
-    fpp::Packet input_packet { fpp::MediaType::Unknown };
+    fpp::Packet input_packet {
+        fpp::MediaType::Unknown
+    };
     const auto read_packet {
-        [&input_packet,&camera]() {
-            input_packet = camera.read();
+        [&input_packet,&source]() {
+            input_packet = source.read();
             return !input_packet.isEOF();
         }
     };
@@ -149,51 +171,47 @@ void startYoutubeStream() {
     /* read and write packets */
     while (read_packet()) {
         if (input_packet.isVideo()) {
-            if (transcode_video) {
-                const auto video_frames { video_decoder.decode(input_packet) };
-                for (const auto& v_frame : video_frames) {
-                    const auto video_packets { video_encoder.encode(v_frame) };
-                    for (const auto& v_packet : video_packets) {
-                        youtube.write(v_packet);
-                    }
-                }
+            if (video_transcoding_required) {
+                for (const auto& v_frame  : video_decoder.decode(input_packet)) {
+                for (const auto& v_packet : video_encoder.encode(v_frame))      {
+                    youtube.write(v_packet);
+                }}
             }
             else {
                 youtube.write(input_packet);
             }
         }
         else if (input_packet.isAudio()) {
-            const auto audio_frames { audio_decoder.decode(input_packet) };
-            for (const auto& a_frame : audio_frames) {
-                const auto resampled_frames { resample.resample(a_frame) };
-                for (auto& ra_frame : resampled_frames) {
-                    auto audio_packets { audio_encoder.encode(ra_frame) };
-                    for (auto& a_packet : audio_packets) {
-                        youtube.write(a_packet);
-                    }
-                }
-            }
-        }
-        else {
-            throw std::runtime_error { "Unknown packet! "};
+            for (const auto& a_frame  : audio_decoder.decode(input_packet)) {
+            for (const auto& ra_frame : resample.resample(a_frame))         {
+            for (const auto& a_packet : audio_encoder.encode(ra_frame))     {
+                youtube.write(a_packet);
+            }}}
         }
     }
 
-#undef FPP_DEBUG
+    /* explicitly close contexts */
+    source.close();
+    youtube.close();
+
 }
 
 void rtp_video_and_audio_stream() {
 
+    const std::string ip {
+        "127.0.0.1"
+    };
+
     /* create source */
-    fpp::InputFormatContext camera { "rtsp://admin:admin@192.168.10.3:554" };
+    fpp::InputFormatContext source {
+        "rtsp://admin:admin@192.168.10.3:554"
+    };
 
     /* open source */
-    camera.open();
-
-    const std::string ip { "127.0.0.1" };
+    source.open();
 
     /* create video sink */
-    const auto rtp_port_video  { 700 };
+    const auto rtp_port_video  { 16700 };
     const auto rtcp_port_video { rtp_port_video + 1 };
     fpp::OutputFormatContext rtp_video_restreamer {
         "rtp://" + ip + ":" + std::to_string(rtp_port_video)
@@ -201,7 +219,7 @@ void rtp_video_and_audio_stream() {
     };
 
     /* copy only video stream to 1st sink */
-    rtp_video_restreamer.copyStream(camera.stream(fpp::MediaType::Video));
+    rtp_video_restreamer.copyStream(source.stream(fpp::MediaType::Video));
 
     /* create audio sink */
     const auto rtp_port_audio  { rtp_port_video + 2 };
@@ -212,37 +230,39 @@ void rtp_video_and_audio_stream() {
     };
 
     /* copy only audio stream to 2nd sink */
-    rtp_audio_restreamer.copyStream(camera.stream(fpp::MediaType::Audio));
-    /* fix  */
+    rtp_audio_restreamer.copyStream(source.stream(fpp::MediaType::Audio));
+    /* fix audio stream index
+     * (so that the player can distinguish video and audio stream) */
     rtp_audio_restreamer.stream(fpp::MediaType::Audio)->setIndex(1);
 
     /* open sinks */
     rtp_video_restreamer.open();
     rtp_audio_restreamer.open();
 
+    /* create sdp file */
     const auto sdp {
         fpp::utils::merge_sdp_files(
             rtp_video_restreamer.sdp()
             , rtp_audio_restreamer.sdp()
         )
     };
-
-    /* create sdp file */
     std::ofstream sdp_file;
     sdp_file.open("video_and_audio.sdp");
     sdp_file << sdp;
     sdp_file.close();
 
-    fpp::Packet input_packet { fpp::MediaType::Unknown };
-    const auto read_video_packet {
-        [&input_packet,&camera]() {
-            input_packet = camera.read();
+    fpp::Packet input_packet {
+        fpp::MediaType::Unknown
+    };
+    const auto read_packet {
+        [&input_packet,&source]() {
+            input_packet = source.read();
             return !input_packet.isEOF();
         }
     };
 
     /* read and write packets */
-    while (read_video_packet()) {
+    while (read_packet()) {
         if (input_packet.isVideo()) {
             rtp_video_restreamer.write(input_packet);
         }
@@ -252,21 +272,26 @@ void rtp_video_and_audio_stream() {
         }
     }
 
+    /* explicitly close contexts */
+    source.close();
+    rtp_video_restreamer.close();
+    rtp_audio_restreamer.close();
+
 }
 
 void rtp_video_stream() {
 
     /* create source */
-    fpp::InputFormatContext camera {
+    fpp::InputFormatContext source {
         "rtsp://admin:admin@192.168.10.3:554"
     };
 
     /* open source */
-    camera.open();
+    source.open();
 
     /* create sink */
     const std::string ip { "127.0.0.1" };
-    const auto rtp_port  { 700 };
+    const auto rtp_port  { 16700 };
     const auto rtcp_port { rtp_port + 1 };
     fpp::OutputFormatContext rtp_restreamer {
         "rtp://" + ip + ":" + std::to_string(rtp_port)
@@ -274,7 +299,7 @@ void rtp_video_stream() {
     };
 
     /* copy only video stream to sink */
-    rtp_restreamer.copyStream(camera.stream(fpp::MediaType::Video));
+    rtp_restreamer.copyStream(source.stream(fpp::MediaType::Video));
 
     /* open sink */
     rtp_restreamer.open();
@@ -285,11 +310,13 @@ void rtp_video_stream() {
     sdp_file << rtp_restreamer.sdp();
     sdp_file.close();
 
-    fpp::Packet input_packet { fpp::MediaType::Unknown };
+    fpp::Packet input_packet {
+        fpp::MediaType::Unknown
+    };
     const auto read_video_packet {
-        [&input_packet,&camera]() {
+        [&input_packet,&source]() {
             do {
-                input_packet = camera.read();
+                input_packet = source.read();
             } while (!input_packet.isVideo() && !input_packet.isEOF());
             return !input_packet.isEOF();
         }
@@ -300,21 +327,25 @@ void rtp_video_stream() {
         rtp_restreamer.write(input_packet);
     }
 
+    /* explicitly close contexts */
+    source.close();
+    rtp_restreamer.close();
+
 }
 
 void rtp_audio_stream() {
 
     /* create source */
-    fpp::InputFormatContext camera {
+    fpp::InputFormatContext source {
         "rtsp://admin:admin@192.168.10.3:554"
     };
 
     /* open source */
-    camera.open();
+    source.open();
 
     /* create sink */
     const std::string ip { "127.0.0.1" };
-    const auto rtp_port  { 700 };
+    const auto rtp_port  { 16700 };
     const auto rtcp_port { rtp_port + 1 };
     fpp::OutputFormatContext rtp_restreamer {
         "rtp://" + ip + ":" + std::to_string(rtp_port)
@@ -322,7 +353,7 @@ void rtp_audio_stream() {
     };
 
     /* copy only audio stream to sink */
-    rtp_restreamer.copyStream(camera.stream(fpp::MediaType::Audio));
+    rtp_restreamer.copyStream(source.stream(fpp::MediaType::Audio));
 
     /* open sink */
     rtp_restreamer.open();
@@ -333,11 +364,13 @@ void rtp_audio_stream() {
     sdp_file << rtp_restreamer.sdp();
     sdp_file.close();
 
-    fpp::Packet input_packet { fpp::MediaType::Unknown };
+    fpp::Packet input_packet {
+        fpp::MediaType::Unknown
+    };
     const auto read_audio_packet {
-        [&input_packet,&camera]() {
+        [&input_packet,&source]() {
             do {
-                input_packet = camera.read();
+                input_packet = source.read();
             } while (!input_packet.isAudio() && !input_packet.isEOF());
             return !input_packet.isEOF();
         }
@@ -345,28 +378,30 @@ void rtp_audio_stream() {
 
     /* read and write packet */
     while (read_audio_packet()) {
+        /* fix audio packet's stream index */
         input_packet.setStreamIndex(0);
         rtp_restreamer.write(input_packet);
     }
 
+    /* explicitly close contexts */
+    source.close();
+    rtp_restreamer.close();
+
 }
 
-int main() {
+auto main() -> int {
+
+    std::cout << "Started\n";
 
     try {
 
-//        #pragma warning( push )
-//        #pragma warning( disable : 4974)
-        ::av_register_all();
-//        #pragma warning( pop )
-        ::avformat_network_init();
         ::avdevice_register_all();
 
-//        startYoutubeStream();
-//        simpleCopyFile();
-        rtp_video_and_audio_stream();
+//        transmuxing_file();
+        youtube_stream();
 //        rtp_video_stream();
 //        rtp_audio_stream();
+//        rtp_video_and_audio_stream();
 
     } catch (const fpp::FFmpegException& e) {
         std::cout << "FFmpegException: " << e.what() << "\n";
