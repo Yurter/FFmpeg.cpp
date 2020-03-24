e3519fa5a0332c8d1dd9749020d34d5a4500117f
Yurter <sterben_idd@mail.ru>
2020-03-24 16:02:44 +0300

    [Added] text_on_video() ex func in main.cpp

--- main.cpp
+++ main.cpp
@@ -5,28 +5,25 @@
 #include <fpp/codec/DecoderContext.hpp>
 #include <fpp/codec/EncoderContext.hpp>
 #include <fpp/refi/ResampleContext.hpp>
+#include <fpp/refi/VideoFilterContext.hpp>
+#include <fpp/refi/VideoFilters/DrawText.hpp>
 #include <fpp/core/Utils.hpp>
 
-extern "C" {
-    #include <libavformat/avformat.h>
-    #include <libavdevice/avdevice.h>
-}
-
 /* USB video
-video=HP Wide Vision FHD Camera
-video=HP Wide Vision HD
-video=Webcam C170
-video=USB2.0 PC CAMERA
+    video=HP Wide Vision FHD Camera
+    video=HP Wide Vision HD
+    video=Webcam C170
+    video=USB2.0 PC CAMERA
 */
 
 /* USB audio
-audio=Набор микрофонов (Realtek High Definition Audio)
+    audio=Набор микрофонов (Realtek High Definition Audio)
 */
 
 /* RTSP
-rtsp://admin:admin@192.168.10.189:554/ch01.264
-rtsp://admin:Admin2019@192.168.10.12:554
-rtsp://admin:admin@192.168.10.3:554 (1080p + audio)
+    rtsp://admin:admin@192.168.10.189:554/ch01.264
+    rtsp://admin:Admin2019@192.168.10.12:554
+    rtsp://admin:admin@192.168.10.3:554 (1080p + audio)
 */
 
 void transmuxing_file() {
@@ -167,6 +164,8 @@ void youtube_stream() {
             return !input_packet.isEOF();
         }
     };
+
+    source.stream(0)->setEndTimePoint(10 * 60 * 1000);
 
     /* read and write packets */
     while (read_packet()) {
@@ -389,6 +388,106 @@ void rtp_audio_stream() {
 
 }
 
+void text_on_video() {
+
+    /* create source */
+    fpp::InputFormatContext source {
+        "rtsp://admin:admin@192.168.10.3:554"
+    };
+
+    /* open source */
+    source.open();
+
+    /* create sink */
+    fpp::OutputFormatContext sink {
+        "text_on_video.flv"
+    };
+
+    /* copy only video stream to sink */
+    for (const auto& input_stream : source.streams()) {
+        if (input_stream->isVideo()) {
+            sink.copyStream(input_stream);
+        }
+    }
+
+    /* create decoder */
+    fpp::DecoderContext video_decoder {
+        source.stream(fpp::MediaType::Video)
+    };
+
+    /* encoder's options */
+    fpp::Options video_options {
+        { "threads",        "1"          }
+        , { "thread_type",  "slice"       }
+        , { "preset",       "ultrafast"   }
+        , { "crf",          "30"          } // 0-51
+        , { "profile",      "main"        }
+        , { "tune",         "zerolatency" }
+    };
+
+    /* create encoder */
+    fpp::EncoderContext video_encoder {
+        sink.stream(fpp::MediaType::Video), video_options
+    };
+
+    const auto drow_text {
+        fpp::VideoFilter::DrawText::make(
+            fpp::VideoFilter::DrawText::Text {
+                "Hello World"   /* text      */
+                , ""            /* text file */
+                , "100"         /* x         */
+                , "100"         /* y         */
+            }
+            , fpp::VideoFilter::DrawText::Font {
+                ""              /* font */
+                , ""            /* font file */
+                , "36"          /* font size */
+                , "red"         /* color */
+                , ""            /* fontcolor_expr */
+            }
+        )
+    };
+
+    /* create filter */
+    fpp::VideoFilterContext video_filter {
+        source.stream(fpp::MediaType::Video)->params
+        , drow_text
+    };
+
+    /* open sink */
+    sink.open();
+
+    /* set timeout (because of endless rtsp stream) */
+    const auto one_minute { 1 * 60 * 1000 };
+    source.stream(fpp::MediaType::Video)->setEndTimePoint(one_minute);
+
+    fpp::Packet input_packet {
+        fpp::MediaType::Unknown
+    };
+    const auto read_packet {
+        [&input_packet,&source]() {
+            input_packet = source.read();
+            return !input_packet.isEOF();
+        }
+    };
+
+    /* read and write packets */
+    while (read_packet()) {
+        if (input_packet.isVideo()) {
+            for (const auto& v_frame  : video_decoder.decode(input_packet)) {
+            for (const auto& f_frame  : video_filter.filter(v_frame))       {
+            for (const auto& v_packet : video_encoder.encode(f_frame))      {
+                sink.write(v_packet);
+            }}}
+        }
+    }
+
+    /* explicitly close contexts */
+    source.close();
+    sink.close();
+
+}
+
 auto main() -> int {
 
     std::cout << "Started\n";
@@ -400,6 +499,7 @@ auto main() -> int {
 //        rtp_video_stream();
 //        rtp_audio_stream();
 //        rtp_video_and_audio_stream();
+//        text_on_video();
 
     } catch (const fpp::FFmpegException& e) {
         std::cout << "FFmpegException: " << e.what() << "\n";
