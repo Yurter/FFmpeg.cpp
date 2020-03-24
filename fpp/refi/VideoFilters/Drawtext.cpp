4978c24e88b217eae35ff764c65a34bef383946a
Yurter <sterben_idd@mail.ru>
2020-03-24 15:50:40 +0300

    [Edited] DrawText

--- fpp/refi/VideoFilters/Drawtext.cpp
+++ fpp/refi/VideoFilters/Drawtext.cpp
@@ -2,33 +2,30 @@
 
 namespace fpp::VideoFilter::DrawText {
 
-    //R"(drawtext=fontfile=/path/to/font.ttf: text='Stack Overflow': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2)"
-    //drawtext=text='Stack Overflow': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2
-    //drawtext=text='Hello World': x=100: y=500:
-    std::string make(VideoFilter::DrawText::Text text
-                    , VideoFilter::DrawText::Font font
-                    , VideoFilter::DrawText::Box box
-                    ) {
+    std::string make(Text text, Font font, Box box, Shadow shadow, Time time) {
+
         std::string filter_description {
             "drawtext="
         };
-        const auto wrap_if_has_spaces {
-            [](std::string& value) {
-                if (const auto pos { value.find(' ') }; pos != std::string::npos) {
-                    value = "'" + value + "'";
-                }
-            }
-        };
+        const std::string param_delim { ": " };
         const auto create_param {
-            [wrap_if_has_spaces](const std::string& key, std::string value) {
-                wrap_if_has_spaces(value);
-                return key + "=" + value + ": ";
+            [param_delim](const std::string& key, const std::string& value) {
+                return key + "=" + value + param_delim;
             }
         };
         const auto append_param {
             [&filter_description,&create_param]
             (const std::string& key, const std::string& value) {
                 filter_description.append(create_param(key, value));
+            }
+        };
+        const auto trim_last_delim {
+            [param_delim](std::string& str) {
+                if (const auto pos {
+                    str.rfind(param_delim)
+                }; pos != std::string::npos) {
+                    str.erase(pos, param_delim.length());
+                }
             }
         };
 
@@ -49,8 +46,37 @@ namespace fpp::VideoFilter::DrawText {
         parse_param(text, y);
 
         // Font
+        parse_param(font, font);
+        parse_param(font, fontfile);
+        parse_param(font, fontsize);
+        parse_param(font, fontcolor);
+        parse_param(font, fontcolor_expr);
+        parse_param(font, alpha);
+        parse_param(font, ft_load_flags);
+
+        //Box
+        parse_param(box, box);
+        parse_param(box, boxcolor);
+        parse_param(box, borderw);
+
+        //Shadow
+        parse_param(shadow, shadowcolor);
+        parse_param(shadow, shadowx);
+        parse_param(shadow, shadowy);
+
+        //Time
+        parse_param(time, timecode);
+        parse_param(time, timecode_rate);
+        parse_param(time, tc24hmax);
+
+        #undef param_setted
+        #undef make_param_pair
+        #undef parse_param
+
+        trim_last_delim(filter_description);
 
         return filter_description;
+
     }
 
 } // namespace fpp::VideoFilter::DrawText
