e0ba95ac28904e934ba332eb95f7ba196b84c289
Yurter <sterben_idd@mail.ru>
2020-04-23 12:39:14 +0600

    [Edited] youtube_stream_transcode()

--- examples/youtube_stream_transcode.cpp
+++ examples/youtube_stream_transcode.cpp
@@ -1,15 +1,12 @@
-#include <iostream>
-#include <fstream>
+#include "examples.hpp"
+
 #include <fpp/context/InputFormatContext.hpp>
 #include <fpp/context/OutputFormatContext.hpp>
 #include <fpp/codec/DecoderContext.hpp>
 #include <fpp/codec/EncoderContext.hpp>
 #include <fpp/refi/ResampleContext.hpp>
 #include <fpp/refi/RescaleContext.hpp>
-#include <fpp/refi/VideoFilterContext.hpp>
-#include <fpp/refi/VideoFilters/DrawText.hpp>
 #include <fpp/core/Utils.hpp>
-#include "examples.hpp"
 
 void youtube_stream_transcode() {
 
@@ -19,7 +16,9 @@ void youtube_stream_transcode() {
     };
 
     /* open source */
-    source.open();
+    if (!source.open()) {
+        return;
+    }
 
     /* check input streams */
     if (!source.stream(fpp::MediaType::Video)) {
@@ -39,7 +38,7 @@ void youtube_stream_transcode() {
 
     /* create sink */
     const std::string stream_key {
-        "w24s-620c-21kg-4vy7"
+        "aaaa-bbbb-cccc-dddd"
     };
     fpp::OutputFormatContext sink {
         "rtmp://a.rtmp.youtube.com/live2/"
@@ -68,12 +67,8 @@ void youtube_stream_transcode() {
         , { "thread_type",  "slice"       }
         , { "preset",       "ultrafast"   }
         , { "crf",          "30"          } // 0-51
-//        , { "profile",      "main"        }
+        , { "profile",      "main"        }
         , { "tune",         "zerolatency" }
-    };
-
-    fpp::Options audio_options {
-        { "preset", "low" }
     };
 
     /* create encoders */
@@ -81,8 +76,14 @@ void youtube_stream_transcode() {
         sink.stream(fpp::MediaType::Video)->params, video_options
     };
     fpp::EncoderContext audio_encoder {
-        sink.stream(fpp::MediaType::Audio)->params, audio_options
+        sink.stream(fpp::MediaType::Audio)->params
     };
+
+    /* create rescaler */
+    fpp::RescaleContext rescaler {{
+        source.stream(fpp::MediaType::Video)->params
+        , sink.stream(fpp::MediaType::Video)->params
+    }};
 
     /* create resampler */
     fpp::ResampleContext resample {{
@@ -91,42 +92,33 @@ void youtube_stream_transcode() {
     }};
 
     /* open sink */
-    sink.open();
+    if (!sink.open()) {
+        return;
+    }
 
-    const auto video_transcoding_required {
-        fpp::utils::transcoding_required({
-            source.stream(fpp::MediaType::Video)->params
-            , sink.stream(fpp::MediaType::Video)->params
-        })
-    };
-
-    fpp::Packet input_packet {
+    fpp::Packet packet {
         fpp::MediaType::Unknown
     };
     const auto read_packet {
-        [&input_packet,&source]() {
-            input_packet = source.read();
-            return !input_packet.isEOF();
+        [&packet,&source]() {
+            packet = source.read();
+            return !packet.isEOF();
         }
     };
 
     /* read and write packets */
     while (read_packet()) {
-        if (input_packet.isVideo()) {
-            if (video_transcoding_required) {
-                for (const auto& v_frame  : video_decoder.decode(input_packet)) {
-                for (const auto& v_packet : video_encoder.encode(v_frame))      {
-                    sink.write(v_packet);
-                }}
-            }
-            else {
-                sink.write(input_packet);
-            }
+        if (packet.isVideo()) {
+            for (const auto& v_frame  : video_decoder.decode(packet))   {
+                 const auto& rv_frame { rescaler.scale(v_frame) };
+            for (const auto& v_packet : video_encoder.encode(rv_frame)) {
+                sink.write(v_packet);
+            }}
         }
-        else if (input_packet.isAudio()) {
-            for (const auto& a_frame  : audio_decoder.decode(input_packet)) {
-            for (const auto& ra_frame : resample.resample(a_frame))         {
-            for (const auto& a_packet : audio_encoder.encode(ra_frame))     {
+        else if (packet.isAudio()) {
+            for (const auto& a_frame  : audio_decoder.decode(packet))   {
+            for (const auto& ra_frame : resample.resample(a_frame))     {
+            for (const auto& a_packet : audio_encoder.encode(ra_frame)) {
                 sink.write(a_packet);
             }}}
         }
