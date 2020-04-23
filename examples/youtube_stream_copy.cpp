64e818fe3d7b05fd34a9cae9d78238e008ccffbf
Yurter <sterben_idd@mail.ru>
2020-04-23 12:39:53 +0600

    [Cleanup] youtube_stream_copy()

--- examples/youtube_stream_copy.cpp
+++ examples/youtube_stream_copy.cpp
@@ -1,15 +1,7 @@
-#include <iostream>
-#include <fstream>
+#include "examples.hpp"
+
 #include <fpp/context/InputFormatContext.hpp>
 #include <fpp/context/OutputFormatContext.hpp>
-#include <fpp/codec/DecoderContext.hpp>
-#include <fpp/codec/EncoderContext.hpp>
-#include <fpp/refi/ResampleContext.hpp>
-#include <fpp/refi/RescaleContext.hpp>
-#include <fpp/refi/VideoFilterContext.hpp>
-#include <fpp/refi/VideoFilters/DrawText.hpp>
-#include <fpp/core/Utils.hpp>
-#include "examples.hpp"
 
 void youtube_stream_copy() {
 
