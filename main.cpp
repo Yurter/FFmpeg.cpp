1fc44c139b31e3bded4b4039461aa0bff8a6275a
Yurter <sterben_idd@mail.ru>
2020-04-29 16:59:35 +0300

    [Added] FFmpeg error log level in main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ auto main() -> int {
 
     fpp::static_log_info("main", "Started");
     fpp::static_log_info("main", "FFmpeg version ", fpp::utils::ffmpeg_version());
+    fpp::set_ffmpeg_log_level(fpp::LogLevel::Error);
 
     try {
 
