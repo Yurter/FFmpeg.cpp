fab26077c8981aa16f26abfdddae636c1df1f756
Yurter <sterben_idd@mail.ru>
2021-03-07 14:43:42 +0300

    [Cleanup]

--- fpp/core/Utils.cpp
+++ fpp/core/Utils.cpp
@@ -353,31 +353,6 @@ namespace fpp {
         return result;
     }
 
-    int av_copy_packet_side_data(AVPacket *pkt, const AVPacket *src)
-    {
-        if (src->side_data_elems) {
-            int i;
-            DUP_DATA(pkt->side_data, src->side_data,
-                    src->side_data_elems * sizeof(*src->side_data), 0, ALLOC_MALLOC);
-            if (src != pkt) {
-                memset(pkt->side_data, 0,
-                       src->side_data_elems * sizeof(*src->side_data));
-            }
-            for (i = 0; i < src->side_data_elems; i++) {
-                DUP_DATA(pkt->side_data[i].data, src->side_data[i].data,
-                        src->side_data[i].size, 1, ALLOC_MALLOC);
-                pkt->side_data[i].size = src->side_data[i].size;
-                pkt->side_data[i].type = src->side_data[i].type;
-            }
-        }
-        pkt->side_data_elems = src->side_data_elems;
-        return 0;
-
-    failed_alloc:
-        av_packet_unref(pkt);
-        return AVERROR(ENOMEM);
-    }
-
     bytes utils::serializeAVPacket(const AVPacket& packet) {
         bytes result;
 
